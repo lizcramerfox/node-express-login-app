0e01b91ad59626d3c128df4afb2bc44f68d20f8e
lizcramerfox <ecfox2008@gmail.com>
2021-03-16 17:22:30 -0400

    Replaces deprecated bodyParser

--- index.js
+++ index.js
@@ -5,10 +5,10 @@ const port = 3000
 
 const db = require('./queries')
 
-app.use(bodyParser.json())
+app.use(express.json())
 
 app.use(
-  bodyParser.urlencoded({
+  express.urlencoded({
     extended: true,
   })
 )
